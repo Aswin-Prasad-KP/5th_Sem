2b3c1dbe5543b08cb3ddc2f5cdd8e9d7b4879081
Aswin-Prasad-KP <aswinprasad@student.tce.edu>
2024-07-06 21:02:45 +0530

    BFS, DFS

---
+++ AI/BFS.py
@@ -0,0 +1,31 @@
+from collections import defaultdict;
+
+class Graph:
+    def __init__(self):
+        self.g = defaultdict(list)
+
+    def addEdge(self, s, d):
+        self.g[s].append(d)
+        self.g[d].append(s)
+
+    def BFS(self, s):
+        vis = [False] * (max(self.g) + 1)
+        q = []
+        q.append(s)
+        vis[s] = True
+        while q:
+            s = q.pop(0)
+            print(s, end=" ")
+            for i in self.g[s]:
+                if not vis[i]:
+                    q.append(i)
+                    vis[i] = True
+
+if __name__ == '__main__':
+    N = int(input("Enter no. of Edges : "))
+    G = Graph()
+    print("Enter edges (Source Destination) : ")
+    for i in range(N):
+        s, d = map(int, input().split())
+        G.addEdge(s, d)
+    G.BFS(int(input("Enter Source to Search (BFS) : ")))
---
+++ AI/DFS.py
@@ -0,0 +1,29 @@
+from collections import defaultdict;
+
+class Graph:
+    def __init__(self):
+        self.g = defaultdict(list)
+
+    def addEdge(self, s, d):
+        self.g[s].append(d)
+        self.g[d].append(s)
+
+    def DFS_Util(self, s, vis):
+        vis.add(s)
+        print(s, end=" ")
+        for i in self.g[s]:
+            if i not in vis:
+                self.DFS_Util(i, vis)
+
+    def DFS(self, s):
+        vis = set()
+        self.DFS_Util(s, vis)
+
+if __name__ == '__main__':
+    N = int(input("Enter no. of Edges : "))
+    G = Graph()
+    print("Enter edges (Source Destination) : ")
+    for i in range(N):
+        s, d = map(int, input().split())
+        G.addEdge(s, d)
+    G.DFS(int(input("Enter Source to Search (BFS) : ")))
